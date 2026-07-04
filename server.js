35ad5ea1ffe9b0bf340cfa40bdd1c25a5ddcb8f3
gaganchaudary4421-ops <gaganchaudhary4421@gmail.com>
2026-07-04 23:15:30 +0530

    add comments

--- server.js
+++ server.js
@@ -1,4 +1,6 @@
-const app = require("/src/app");
+//server.js
+const app = require("./src/app");
 app.listen(3000, () => {
+  //starting the server at port 3000
   console.log("Server is running at port 3000");
 });
