63057f35554c3b1aff57a4ca4454fb477a676351
gaganchaudary4421-ops <gaganchaudhary4421@gmail.com>
2026-07-04 23:14:21 +0530

    add comments

--- src/app.js
+++ src/app.js
@@ -1,4 +1,6 @@
 //app.JS
-const express = require("express");
+const express = require("express"); //accessing express package
 const app = express();
 module.exports = app;
+const notes = [];
+app.post("/notes", () => {}); //Api call to create a note
