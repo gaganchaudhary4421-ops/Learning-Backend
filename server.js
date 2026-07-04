1bd91b7ae3d41de4af0bf4a15d510286a913d855
gaganchaudary4421-ops <gaganchaudhary4421@gmail.com>
2026-07-04 23:17:24 +0530

    update server.js

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 //server.js
-const app = require("./src/app");
+const app = require("./src/app"); //accessing app.js file
 app.listen(3000, () => {
   //starting the server at port 3000
   console.log("Server is running at port 3000");
