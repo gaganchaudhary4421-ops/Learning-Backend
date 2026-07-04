95573b48fee3eaa6ee14f8bb8b2bbbca1aefc905
gaganchaudary4421-ops <gaganchaudhary4421@gmail.com>
2026-07-04 23:19:47 +0530

    update app.js

--- src/app.js
+++ src/app.js
@@ -1,6 +1,6 @@
 //app.JS
 const express = require("express"); //accessing express package
-const app = express();
+const app = express(); //creating an instance of express
 module.exports = app;
 const notes = [];
 app.post("/notes", () => {}); //Api call to create a note
