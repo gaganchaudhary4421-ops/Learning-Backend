8d1290f8c0b6ccf429eea231e8249e91300f7938
gaganchaudary4421-ops <gaganchaudhary4421@gmail.com>
2026-07-10 11:37:42 +0530

    Add delete method

--- src/app.js
+++ src/app.js
@@ -1,13 +1,33 @@
-//app.JS
-const express = require("express"); //accessing express package
-const app = express(); //creating an instance of express
+const express = require("express");
+const app = express();
 app.use(express.json());
-
 const notes = [];
 app.post("/notes", (req, res) => {
   notes.push(req.body);
   res.status(201).send({
     message: "Note created successfully",
   });
-}); //Api call to create a note
+});
+app.get("/notes", (req, res) => {
+  res.status(200).json({
+    message: "Notes fetched successfully",
+    notes: notes,
+  });
+});
+app.delete("/notes/:index", (req, res) => {
+  const index = req.params.index;
+
+  delete notes[index];
+  res.status(200).json({
+    message: "Note deleted successfully",
+  });
+});
+app.patch("/notes/:index", (req, res) => {
+  const index = req.params.index;
+  const description = req.body.description;
+  ((notes[index].description = description),
+    res.status(200).json({
+      message: "Note update successfully",
+    }));
+});
 module.exports = app;
