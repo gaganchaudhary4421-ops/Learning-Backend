4ed2dcfb170d7678d36ccdeb5e93bdfd6f1070d9
gaganchaudary4421-ops <gaganchaudhary4421@gmail.com>
2026-07-10 22:45:58 +0530

    Add database file

---
+++ src/db/db.js
@@ -0,0 +1,6 @@
+const mongoose = require("mongoose");
+async function connectDB() {
+  await mongoose.connect(
+    "mongodb+srv://complete-backend:Ko9hkV4dhsOjgI5o@complete-backend.tugcp67.mongodb.net/",
+  );
+}
