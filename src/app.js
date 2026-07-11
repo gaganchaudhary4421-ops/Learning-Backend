42189a31643e478fb6395c9769f39b342665e12b
gaganchaudary4421-ops <gaganchaudhary4421@gmail.com>
2026-07-11 15:47:31 +0530

    Add post method

--- src/app.js
+++ src/app.js
@@ -1,3 +1,16 @@
 const express = require("express");
+const noteModel = require("./models/note.model");
 const app = express();
+app.use(express.json());
+app.post("/notes", async (req, res) => {
+  const data = req.body;
+  await noteModel.create({
+    title: data.title,
+    description: data.description,
+  });
+  res.status(201).send({
+    message: "Note created successfully",
+  });
+});
+
 module.exports = app;
