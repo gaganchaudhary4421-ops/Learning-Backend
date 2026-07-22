c8f0b3b5f6f05ea12f94f82ed5a0f56262945134
gaganchaudary4421-ops <gaganchaudhary4421@gmail.com>
2026-07-22 22:28:02 +0530

    Update server.js

--- server.js
+++ server.js
@@ -2,6 +2,6 @@ const app = require("./src/app");//importing app
 const connectDB = require("./src/db/db.js");
 
 connectDB();
-app.listen(3000, () => {//port no. 3000
+app.listen(3000, () => {//port no. 300
   console.log("server is running on port 3000"); //this message will be displayed.
 });
