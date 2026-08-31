df3f60012ce66b53374e2fd69f999c8bef24ca11
gaganchaudary4421-ops <gaganchaudhary4421@gmail.com>
2026-08-31 20:24:42 +0530

    Start learning express js

--- server.js
+++ server.js
@@ -1,8 +1,3 @@
-const app = require("./src/app"); //importing app
-const connectDB = require("./src/db/db.js");
-
-connectDB();
-app.listen(3000, () => {
-  //port no. 3000
-  console.log("server is running on port 3000"); //this message will be displayed.
-});
+const express = require("express");
+const app = express();
+app.listen(3000);
