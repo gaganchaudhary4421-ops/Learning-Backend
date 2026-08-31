9deed744593816c92387c7d79af641c92cbce1ee
gaganchaudary4421-ops <gaganchaudhary4421@gmail.com>
2026-08-31 20:30:29 +0530

    Start learning express js

--- server.js
+++ server.js
@@ -3,4 +3,6 @@ const app = express();
 app.get("/", (req, res) => {
   res.send("Hello World...");
 });
-app.listen(3000);
+app.listen(3000, () => {
+  console.log("Server is running on port 3000");
+});
