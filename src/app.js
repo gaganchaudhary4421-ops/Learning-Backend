cf046646a044739fcc08c66e7cdf40fa9c0c3ba9
gaganchaudary4421-ops <gaganchaudhary4421@gmail.com>
2026-09-02 20:24:30 +0530

    Create app

--- src/app.js
+++ src/app.js
@@ -1,12 +1,4 @@
-const express = require("express");// r
-const multer = require("multer");
+const express = require("express");
 const app = express();
-app.use(express.json());
-const upload = multer({ storage: multer.memoryStorage() });
-app.post("/create-post", upload.single("image "), async (req, res) => {
-  console.log(req.body);
-  console.log(req.file);
-});
 
 module.exports = app;
-//Start learning node js
