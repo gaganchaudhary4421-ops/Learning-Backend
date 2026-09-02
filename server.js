02431b6dda839d271c66ccdb78fe834a6e38c17b
gaganchaudary4421-ops <gaganchaudhary4421@gmail.com>
2026-09-02 19:44:18 +0530

    Learning building Api's

--- server.js
+++ server.js
@@ -3,6 +3,9 @@ const app = express();
 app.get("/", (req, res) => {
   res.send("Hello World...");
 });
+app.get("/about", (req, res) => {
+  res.send("About Page");
+});
 app.listen(3000, () => {
   console.log("Server is running on port 3000");
 });
