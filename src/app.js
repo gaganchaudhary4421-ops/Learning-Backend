24279ad8e0ae9bd51ca2ced2055a231b71186d05
gaganchaudary4421-ops <gaganchaudhary4421@gmail.com>
2026-09-04 20:59:01 +0530

    Learning Api

--- src/app.js
+++ src/app.js
@@ -12,4 +12,11 @@ app.get("/notes", (req, res) => {
     notes: notes,
   });
 });
+app.delete("/notes/:index", (req, res) => {
+  const index = req.params.index;
+  delete notes[index];
+  res.status(200).json({
+    message: "Note deleted successfully",
+  });
+});
 module.exports = app;
