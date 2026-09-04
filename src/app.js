885660df19f9454533f640b94cee5caeff1b77f8
gaganchaudary4421-ops <gaganchaudhary4421@gmail.com>
2026-09-04 20:46:23 +0530

    Learning Api

--- src/app.js
+++ src/app.js
@@ -3,6 +3,7 @@ const app = express();
 app.use(express.json());
 const notes = [];
 app.post("/notes", (req, res) => {
-  console.log(req.body);
+  notes.push(req.body);
+  res.status(201).json({ message: "Note created successfully" });
 });
 module.exports = app;
