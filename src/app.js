0b081a62a0029a865c6874171be0e2bbf16099e4
gaganchaudary4421-ops <gaganchaudhary4421@gmail.com>
2026-09-05 16:43:16 +0530

    Learning Patch method

--- src/app.js
+++ src/app.js
@@ -6,12 +6,23 @@ app.post("/notes", (req, res) => {
   notes.push(req.body);
   res.status(201).json({ message: "Note created successfully" });
 });
+
 app.get("/notes", (req, res) => {
   res.status(200).json({
     message: "Notes retrieved successfully",
     notes: notes,
   });
 });
+
+app.patch("/notes/:index", (req, res) => {
+  const index = req.params.index;
+  const description = req.body.description;
+  notes[index].description = description;
+  res.status(200).json({
+    message: "Note updated successfully",
+  });
+});
+
 app.delete("/notes/:index", (req, res) => {
   const index = req.params.index;
   delete notes[index];
