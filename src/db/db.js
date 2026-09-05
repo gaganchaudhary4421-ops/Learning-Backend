92d0445948e59b5e94816981ecbd11dddb7aff51
gaganchaudary4421-ops <gaganchaudhary4421@gmail.com>
2026-09-05 17:46:02 +0530

    solving db connection error

--- src/db/db.js
+++ src/db/db.js
@@ -1,8 +1,17 @@
+const dns = require("dns");
+
+dns.setServers(["8.8.8.8", "1.1.1.1"]);
+
 const mongoose = require("mongoose");
+
 async function connectDB() {
-  await mongoose.connect(
-    "mongodb+srv://Backend:b97PSKjF05hvDbtA@backend.jfrj2xn.mongodb.net/backend",
-  );
-  console.log("Connected to MongoDB");
+  try {
+    await mongoose.connect("YOUR_MONGODB_CONNECTION_STRING");
+
+    console.log("Connected to MongoDB");
+  } catch (error) {
+    console.error("MongoDB connection failed:", error);
+  }
 }
+
 module.exports = connectDB;
