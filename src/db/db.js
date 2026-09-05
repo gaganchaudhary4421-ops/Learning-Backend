550b2b5a956069dad2973c75246356b6c861ff85
gaganchaudary4421-ops <gaganchaudhary4421@gmail.com>
2026-09-05 17:30:18 +0530

    Learning connecting server to database

--- src/db/db.js
+++ src/db/db.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 async function connectDB() {
   await mongoose.connect(
-    "mongodb+srv://Backend:b97PSKjF05hvDbtA@backend.jfrj2xn.mongodb.net/",
+    "mongodb+srv://Backend:b97PSKjF05hvDbtA@backend.jfrj2xn.mongodb.net/backend",
   );
   console.log("Connected to MongoDB");
 }
