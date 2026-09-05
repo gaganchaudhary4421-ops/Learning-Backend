ad87ed5a7094d8c2a9b4dc95c3d964806e638c69
gaganchaudary4421-ops <gaganchaudhary4421@gmail.com>
2026-09-05 17:23:37 +0530

    Learning connecting server to database

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
-const app = require(".src/app.js");
+const app = require("./src/app.js");
+const connectDB = require("./src/db/db.js");
 app.listen(3000, () => {
   console.log("Server is running on port 3000");
 });
