a4b1efa0511a996f0497e579d5e6d7b670365b20
gaganchaudary4421-ops <gaganchaudhary4421@gmail.com>
2026-09-06 16:55:58 +0530

    Learning post api

--- src/app.js
+++ src/app.js
@@ -5,25 +5,13 @@ app.use(express.json());
 const notes = [];
 app.post("/notes", async (req, res) => {
   const data = req.body;
-  await noteModel.create({});
+  await noteModel.create({
+    title = data.title,
+    description = data.description,
+  });
   res.status(201).json({
     message: "Note created successfully",
   });
 });
 
-const express = require("express");
-const noteModel = require("./models/models.js");
-const app = express();
-app.use(express.json());
-const notes = [];
-app.post("/notes", async (req, res) => {
-  const data = req.body;
-  await noteModel.create({
-    title: data.title,
-    description: data.description,
-  });
-  res.status(201).json({
-    message: "Note created successfully",
-  });
-});
-module.exports = app; //
+module.exports = app
