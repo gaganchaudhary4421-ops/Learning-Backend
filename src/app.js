e48a4a8ce82d061f2dbdd8b5ec1ad26d14fcc0a0
gaganchaudary4421-ops <gaganchaudhary4421@gmail.com>
2026-09-07 20:43:26 +0530

    Learning get method

--- src/app.js
+++ src/app.js
@@ -1,17 +1,24 @@
 const express = require("express");
-const noteModel = require("noteModel");
+const noteModel = require("./models/models.js");
 const app = express();
 app.use(express.json());
 const notes = [];
 app.post("/notes", async (req, res) => {
   const data = req.body;
   await noteModel.create({
-    title = data.title,
-    description = data.description,
+    title: data.title,
+    description: data.description,
   });
   res.status(201).json({
     message: "Note created successfully",
   });
 });
-
-module.exports = app
+app.get("/notes", async (req, res) => {
+  const notes = await noteModel.find(
+    res.status(200).json({
+      message: "Note fetched successfully",
+      notes: notes,
+    }),
+  );
+});
+module.exports = app;
