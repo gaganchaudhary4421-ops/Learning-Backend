a6318de426a7524a95450d270e75b1309a39928b
gaganchaudary4421-ops <gaganchaudhary4421@gmail.com>
2026-09-08 20:14:57 +0530

    Practice

--- src/app.js
+++ src/app.js
@@ -1,5 +1,9 @@
 const express = require("express");
 const app = express();
 app.use(express.json());
-app.post("/create-post", (req, res) => {});
+app.post("/create-post", (req, res) => {
+  res.status(201).json({
+    message: "Post created successfully",
+  });
+});
 module.exports = app;
