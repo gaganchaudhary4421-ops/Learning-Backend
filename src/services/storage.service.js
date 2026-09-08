ba0e0e4832180f7f83edae93639dd169943e4368
gaganchaudary4421-ops <gaganchaudhary4421@gmail.com>
2026-09-08 21:08:58 +0530

    Practice

--- src/services/storage.service.js
+++ src/services/storage.service.js
@@ -1,12 +1,18 @@
-const imagekit = require("@imagekit/nodejs");
-const imagekit = new imagekit({
-  privatekey: "private_fw96EGAkgTBNLjA3R5S/5Nik+Ds=",
+const ImageKit = require("@imagekit/nodejs");
+
+const imagekit = new ImageKit({
+  privateKey: "private_fw96EGAkgTBNLjA3R5S/5Nik+Ds=",
 });
+
 async function uploadFile(buffer) {
-  const result = await imagekit.client.upload({
+  const result = await imagekit.files.upload({
     file: buffer,
     fileName: "image.jpg",
   });
+
   return result;
 }
-module.exports = storageService;
+
+module.exports = {
+  uploadFile,
+};
