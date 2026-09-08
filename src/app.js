608a59c0305790b0c0bf8557a1627e997e47a9ed
gaganchaudary4421-ops <gaganchaudhary4421@gmail.com>
2026-09-08 20:30:12 +0530

    Practice

--- src/app.js
+++ src/app.js
@@ -3,7 +3,7 @@ const multer = require("multer");
 const app = express();
 app.use(express.json());
 const upload = multer({ storage: multer.memoryStorage });
-app.post("/create-post", upload.single("image"), async (req, res) => {
+app.post("/create-post", upload.single("image"), (req, res) => {
   console.log(req.body);
   console.log(req.file);
 
