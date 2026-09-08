620d596870d964868504696046c1bad4f5c533f2
gaganchaudary4421-ops <gaganchaudhary4421@gmail.com>
2026-09-08 20:58:59 +0530

    Practice

--- src/app.js
+++ src/app.js
@@ -9,6 +9,7 @@ const upload = multer({ storage: multer.memoryStorage() });
 app.post("/create-post", upload.single("image"), async (req, res) => {
   console.log(req.body);
   console.log(req.file);
+  const result = await storageService.uploadFile(req.file.buffer);
 
   res.status(201).json({
     message: "Post created successfully",
