65d795fc6f1357130244a7939b39690873afc65e
gaganchaudary4421-ops <gaganchaudhary4421@gmail.com>
2026-09-08 20:04:26 +0530

    Practice

--- src/models/post.model.js
+++ src/models/post.model.js
@@ -1,7 +1,7 @@
-const mongoose = require("mongoose");//requiring mongoose
-const postSchema = new.mongoose.Schema({
-    image: String,
-    caption: String,
-})
-const postModel = mongoose.model("post",postModel);
+const mongoose = require("mongoose");
+const postSchema = new mongoose.Schema({
+  image: String,
+  caption: String,
+});
+const postModel = mongoose.model("post", postSchema);
 module.exports = postModel;
