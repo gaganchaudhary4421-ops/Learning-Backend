622947ec68ffad71852c50d5dfc5be11884357e1
gaganchaudary4421-ops <gaganchaudhary4421@gmail.com>
2026-09-08 19:50:14 +0530

    Practice

--- src/app.js
+++ src/app.js
@@ -1,29 +1,3 @@
 const express = require("express");
-const noteModel = require("./models/models.js");
 const app = express();
-app.use(express.json());
-app.post("/notes", async (req, res) => {
-  const data = req.body;
-  await noteModel.create({
-    title: data.title,
-    description: data.description,
-  });
-  res.status(201).json({
-    message: "Note created successfully",
-  });
-});
-app.get("/notes", async (req, res) => {
-  const notes = await noteModel.find();
-  res.status(200).json({
-    message: "Note fetched successfully",
-    notes: notes,
-  });
-});
-app.delete("/notes/:index", async (req, res) => {
-  const index = req.params.index;
-  await noteModel.findOneAndDelete({ _id: index });
-  res.status(200).json({
-    message: "Note deleted successfully",
-  });
-});
 module.exports = app;
