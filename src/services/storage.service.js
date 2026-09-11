afe4a970b347e4f77bfa2d203efe23689a54cf49
gaganchaudary4421-ops <gaganchaudhary4421@gmail.com>
2026-09-11 21:24:08 +0530

    Aoi

--- src/services/storage.service.js
+++ src/services/storage.service.js
@@ -12,7 +12,7 @@ async function uploadFile(buffer) {
 
   return result;
 }
-//Learning ap
+//Learning api key and private key are used to authenticate the request to the ImageKit API. The uploadFile function takes a buffer as input, which represents the file to be uploaded. It uses the ImageKit SDK to upload the file and returns the result of the upload operation.
 module.exports = {
   uploadFile,
 };
