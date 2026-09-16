2da77f45ab0f55c9923a2a01c3b22f3f28e7103b
gaganchaudary4421-ops <gaganchaudhary4421@gmail.com>
2026-09-16 20:26:07 +0530

    Writing code again

--- src/app.js
+++ src/app.js
@@ -8,6 +8,14 @@ const app = express();
 app.use(express.json());
 app.post("/create-post", upload.single("image"), async (req, res) => {
   const result = await storageService.uploadImage(req.file.buffer);
-  const
+  const postModel = await postModle.create({
+    title: req.body.title,
+    description: req.body.description,
+    imageUrl: result.URL,
+  });
+  res.status(201).json({
+    message: "Post created successfully",
+    post: postModel,
+  });
 });
 module.exports = app;
