5e3e608e143deee886ebf79bd1f8dde103e9f495
gaganchaudary4421-ops <gaganchaudhary4421@gmail.com>
2026-09-16 20:33:20 +0530

    Writing code again

--- src/services/storage.service.js
+++ src/services/storage.service.js
@@ -1,11 +1,18 @@
 const ImageKit = require("@imagekit/nodejs");
+
 const imagekit = new ImageKit({
   privateKey: process.env.IMAGEKIT_PRIVATE_KEY,
 });
-async function upload(Buffer) {
-  const result = await imagekit.upload({
-    file: Buffer.toString("base64"),
+
+async function uploadFile(buffer) {
+  const result = await imagekit.files.upload({
+    file: buffer.toString("base64"),
     fileName: "image.jpg",
   });
+
   return result;
 }
+//Learning API's
+module.exports = {
+  uploadFile,
+};
