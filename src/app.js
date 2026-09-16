1b3dea89cc0b87d145ca81f81aab9a8721a354fc
gaganchaudary4421-ops <gaganchaudhary4421@gmail.com>
2026-09-16 20:19:36 +0530

    Writing code again

--- src/app.js
+++ src/app.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const connectDB = require("./db/db");
-require("dotenv").config();
+const multer = require("multer");
 const app = express();
 app.use(express.json());
 module.exports = app;
