848632f4feed9e42dfcfb97775d22d3441513862
gaganchaudary4421-ops <gaganchaudhary4421@gmail.com>
2026-09-16 20:21:51 +0530

    Writing code again

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const connectDB = require("./db/db");
 const multer = require("multer");
 const storageService = require("./services/storageService");
+const upload = multer({ storage: multer.memoryStorage() });
 const app = express();
 app.use(express.json());
 module.exports = app;
