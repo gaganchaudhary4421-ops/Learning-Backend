2a13995990951099452231a134b6bb42f139e8d3
gaganchaudary4421-ops <gaganchaudhary4421@gmail.com>
2026-09-16 20:24:18 +0530

    Writing code again

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const connectDB = require("./db/db");
+const postModle = require("./models/postModel");
 const multer = require("multer");
 const storageService = require("./services/storageService");
 const upload = multer({ storage: multer.memoryStorage() });
@@ -7,5 +8,6 @@ const app = express();
 app.use(express.json());
 app.post("/create-post", upload.single("image"), async (req, res) => {
   const result = await storageService.uploadImage(req.file.buffer);
+  const
 });
 module.exports = app;
