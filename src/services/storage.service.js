abba0d487c688506b9ff8b612eb170c8721e1416
gaganchaudary4421-ops <gaganchaudhary4421@gmail.com>
2026-09-16 20:32:02 +0530

    Writing code again

--- src/services/storage.service.js
+++ src/services/storage.service.js
@@ -2,4 +2,10 @@ const ImageKit = require("@imagekit/nodejs");
 const imagekit = new ImageKit({
   privateKey: process.env.IMAGEKIT_PRIVATE_KEY,
 });
-async function upload(Buffer) {}
+async function upload(Buffer) {
+  const result = await imagekit.upload({
+    file: Buffer,
+    fileName: "image.jpg",
+  });
+  return result;
+}
