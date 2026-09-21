c0741558ae47eea1c518210fcb46d055da12311a
gaganchaudary4421-ops <gaganchaudhary4421@gmail.com>
2026-09-21 20:07:52 +0530

    Creating get apu

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -19,5 +19,11 @@ app.post("/create-post", upload.single("image"), async (req, res) => {
     post,
   });
 });
-app.get("/posts", async (req, res) => {});
+app.get("/posts", async (req, res) => {
+  const posts = await postModel.find();
+  res.status(200).json({
+    message: "Posts fetched successfully",
+    posts,
+  });
+});
 module.exports = app;
