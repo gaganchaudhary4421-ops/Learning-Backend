a387d7804f18bc1efcf4e32870708d859183d248
gaganchaudary4421-ops <gaganchaudhary4421@gmail.com>
2026-09-21 20:34:22 +0530

    solving database conection error

--- Backend/src/db/db.js
+++ Backend/src/db/db.js
@@ -1,17 +1,14 @@
 require("dotenv").config();
+const dns = require("dns");
+
+dns.setServers(["8.8.8.8", "1.1.1.1"]);
 
 const mongoose = require("mongoose");
 
 async function connectDB() {
-  try {
-    await mongoose.connect(process.env.MONGO_URI);
+  await mongoose.connect(process.env.MONGO_URI);
 
-    console.log("Connected to MongoDB");
-  } catch (error) {
-    console.error("MongoDB connection failed:");
-    console.error(error);
-    process.exit(1);
-  }
+  console.log("Connected to MongoDB");
 }
 
 module.exports = connectDB;
