5e87b4f92c5b6254c001da1afb7d5e1eb1b9e2c5
gaganchaudary4421-ops <gaganchaudhary4421@gmail.com>
2026-09-22 20:39:06 +0530

    Building create post page

--- Frontend/frontend/src/pages/createPost.jsx
+++ Frontend/frontend/src/pages/createPost.jsx
@@ -1,23 +1,24 @@
 import React from "react";
 
-const createPost = () => {
+const CreatePost = () => {
   return (
-    <div>
-      <section className="create-post-section">
-        <h1>Create Post</h1>
-        <form>
-          <input type="file" name="file" accept="image/*" />
-          <input
-            type="text"
-            name="caption"
-            placeholder="Enter Caption"
-            required
-          />
-          <button type="submit">Submit</button>
-        </form>
-      </section>
-    </div>
+    <section className="create-post-section">
+      <h1>Create Post</h1>
+
+      <form>
+        <input type="file" name="file" accept="image/*" />
+
+        <input
+          type="text"
+          name="caption"
+          placeholder="Enter Caption"
+          required
+        />
+
+        <button type="submit">Submit</button>
+      </form>
+    </section>
   );
 };
 
-export default createPost;
+export default CreatePost;
