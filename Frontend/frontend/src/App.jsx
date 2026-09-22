86a48c42a2fd19f4f913c4e9e8f1d22c7ab14530
gaganchaudary4421-ops <gaganchaudhary4421@gmail.com>
2026-09-22 19:56:09 +0530

    Building create post page

--- Frontend/frontend/src/App.jsx
+++ Frontend/frontend/src/App.jsx
@@ -1,7 +1,13 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 const App = () => {
-  return <div>App</div>;
+  return (
+    <Router>
+      <Routes>
+        <Route path="/create-post" element={<CreatePost />} />
+      </Routes>
+    </Router>
+  );
 };
 
 export default App;
