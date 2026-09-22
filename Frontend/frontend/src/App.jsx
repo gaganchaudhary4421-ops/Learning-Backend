f95c7167ad47baf710d75c07e1f96e1353ab0939
gaganchaudary4421-ops <gaganchaudhary4421@gmail.com>
2026-09-22 20:09:32 +0530

    Building create post page

--- Frontend/frontend/src/App.jsx
+++ Frontend/frontend/src/App.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import CreatePost from "./pages/createPost";
-import index from "./index.css";
 const App = () => {
   return (
     <Router>
