e72c9547d5abace3daf2dbe5b5de7c0fcbf78612
gaganchaudary4421-ops <gaganchaudhary4421@gmail.com>
2026-09-23 16:32:12 +0530

    building frontend

--- Frontend/frontend/src/pages/Feed.jsx
+++ Frontend/frontend/src/pages/Feed.jsx
@@ -6,20 +6,18 @@ const Feed = () => {
       _id: "1",
       image:
         "https://images.unsplash.com/photo-1747096241806-82c763e28744?w=900&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHx0b3BpYy1mZWVkfDJ8NnNNVmpUTFNrZVF8fGVufDB8fHx8fA%3D%3D",
-      caption: "A beautiful sunset over the mountains.",
+      caption: "A beautiful sunset",
     },
   ]);
   return (
     <section className="feed-section">
-      posts.length === 0 ? (
+      posts.length === 0 ? (<p>No posts available.</p>) : (
       {posts.map((post) => (
         <div key={post._id} className="post">
           <img src={post.image} alt="Post" />
-          <p>{post.caption}</p>
         </div>
       ))}
-      ):(
-      <h1>No posts available.</h1>)
+      )
     </section>
   );
 };
