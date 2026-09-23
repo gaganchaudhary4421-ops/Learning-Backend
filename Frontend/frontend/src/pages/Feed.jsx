c27a1e26f66aca193eb0e7121e0da36278dd4351
gaganchaudary4421-ops <gaganchaudhary4421@gmail.com>
2026-09-23 16:28:53 +0530

    building frontend

--- Frontend/frontend/src/pages/Feed.jsx
+++ Frontend/frontend/src/pages/Feed.jsx
@@ -6,6 +6,7 @@ const Feed = () => {
       _id: "1",
       image:
         "https://images.unsplash.com/photo-1747096241806-82c763e28744?w=900&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHx0b3BpYy1mZWVkfDJ8NnNNVmpUTFNrZVF8fGVufDB8fHx8fA%3D%3D",
+      caption: "A beautiful sunset over the mountains.",
     },
   ]);
   return (
